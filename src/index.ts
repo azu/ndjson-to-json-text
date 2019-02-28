df6f27e1e936d91cb540ef1ba1cd7496fbf0d263
azu <azuciao@gmail.com>
2019-02-28 19:40:19 +0900

    fix(lib): correct empty line handling

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@
  * @param ndjsonText
  */
 export function ndjsonToJsonText(ndjsonText: string): string {
-    const linesWithComma = ndjsonText.replace(/\r?\n/g, ",");
+    // Remove First and Last empty for avoid to add , in invalid position
+    // For example, JSON.parse does not parse [1,2,3,]
+    const linesWithComma = ndjsonText.trim()
+    // NDJSON may skip empty lines -> handle mulitple new lines as a single new line
+    // https://github.com/ndjson/ndjson-spec#32-parsing
+    .replace(/(!?\r?\n)+/g, ",");
     return `[${linesWithComma}]`;
 }
